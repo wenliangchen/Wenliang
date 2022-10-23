8887502b92ab8f2b2f247b8de53029890503b6f7
Wenliang Chen <wiliamchensde@outlook.com>
2022-10-23 09:42:29 -0500

    update positions

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -6,10 +6,10 @@ const positions = [
     daterange: 'June 2021 - Present',
     points: [
       'Collaborated with 6 teammates to build the remote agriculture harvest system using Java',
-      +' Developed the backend of RESTful API with Spring Boot and managed the MySQL database and collaborate it with Spring Data JPA, Applied JSON for front/back-end Communication',
-      +' Implemented the frontend with React.js and Bootstrap to provide access to configure and launch the system',
-      +' Decreased video streaming latency from 2000ms to 100ms by using WebRTC protocol instead of RTSP',
-      +' Tested backend application with unit and integration testing by using Spock framework in groovy',
+      ' Developed the backend of RESTful API with Spring Boot and managed the MySQL database and collaborate it with Spring Data JPA, Applied JSON for front/back-end Communication',
+      ' Implemented the frontend with React.js and Bootstrap to provide access to configure and launch the system',
+      ' Decreased video streaming latency from 2000ms to 100ms by using WebRTC protocol instead of RTSP',
+      ' Tested backend application with unit and integration testing by using Spock framework in groovy',
     ],
   },
 ];
